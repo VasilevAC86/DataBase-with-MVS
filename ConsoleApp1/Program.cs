39cf56f89acb6ff55c241eadcf3f98bfdd7f810b
Top <Top@WIN-7RSES2G9CV4>
2024-09-04 20:40:25 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "test.db"; // Название БД
+            Console.WriteLine($"Попытка создать базу {name}");
+            Console.WriteLine(DBWork.MakeDB(name)); // Создаём БД и выводим её в консоль
+        }
+    }
+}
